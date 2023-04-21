616f46c8db72b2f10e519232e1f6b5f671414be2
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-04-21 16:58:55 +0530

    Add files via upload

---
+++ Project-Inliner-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b;
+	cout<<"enter number for A->";
+	cin>>a;
+    cout<<"enter number for B->";
+	cin>>b;
+	
+	b=a;
+	cout<<"the final answer is->"<<a;
+}
